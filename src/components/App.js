22ec72ca490e85753a19dbb91f23b245b2770b52
Tyler Collier <tyler.collier+github@devmounta.in>
2018-07-03 14:23:39 -0700

    Add People And NotFound components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import Home from './Home/Home';
 import About from './About/About';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import FAQ from './About/FAQ/FAQ';
 import Company from './About/Company/Company';
+import People from './People/People';
+import Character from './Character/Character';
+
+const NotFound = props => {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <div>You visited an invalid page. Maybe start from the <Link to="/">Home page</Link>?</div>
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
@@ -13,12 +24,18 @@ class App extends Component {
         <nav>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
+          <Link to="/people">People</Link>
         </nav>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" render={() => <About>
-          <Route path="/about/faq" component={FAQ} />
-          <Route path="/about/company" component={Company} />
-        </About>} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" render={() => <About>
+            <Route path="/about/faq" component={FAQ} />
+            <Route path="/about/company" component={Company} />
+          </About>} />
+          <Route path="/people" component={People} exact />
+          <Route path="/people/:id" component={Character} />
+          <Route path="" component={NotFound} />
+        </Switch>
       </div>
     );
   }
--- src/components/People/People.js
+++ src/components/People/People.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getPeople } from '../../usersService.js';
+import { Link } from 'react-router-dom';
 
 export default class People extends Component {
   constructor() {
@@ -18,9 +19,9 @@ export default class People extends Component {
   }
 
   render() {
-    const people = this.state.people.map((e, i) => {
+    const people = this.state.people.map(e => {
       return (
-        <h3>{e.name}</h3>
+        <h3 key={e.id}><Link to={`/people/${e.id}`}>{e.name}</Link></h3>
       )
     })
     return (
